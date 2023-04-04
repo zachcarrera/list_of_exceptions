746b1bffcbb4c0f85da2c189a13c6017000975cd
Zach Carrera <zachcarrera@gmail.com>
2023-04-03 23:27:35 -0700

    add TestExceptions.java

---
+++ TestExceptions.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+public class TestExceptions {
+    public static void main(String[] args) {
+        ArrayList<Object> myList = new ArrayList<Object>();
+        myList.add("13");
+        myList.add("hello world");
+        myList.add(48);
+        myList.add("Goodbye World");
+
+        for(int i = 0; i < myList.size(); i++) {
+            try {
+                Integer castedValue = (Integer) myList.get(i);
+            } catch (Exception e) {
+                System.out.println(e);
+                System.out.println("There was a problem at index " + i + " with value " + myList.get(i));
+            }
+        }
+    }
+}
